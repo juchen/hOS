163497794b44f5b787af2868901942cec3488de0
NVT02080 <Henry_Chen@novatek.com.tw>
2014-12-09 11:05:31 +0800

    Remove some dead code.

--- src/pm_c_entry.cpp
+++ src/pm_c_entry.cpp
@@ -80,11 +80,6 @@ static inline void init_IntCtrlr(void)
 	outb(PIC2_DATA, 0xFF /*a2*/);
 }
 
-static inline void enable_Int()
-{
-	asm("sti\n\t");
-}
-
 char idle_stack[4096];
 void idle(void *parm)
 {
@@ -112,150 +107,129 @@ private:
 #define SEGS_BASE 0x00000000
 void pm_c_entry(void)
 {
-  ComPort comPort;
-  //putChar_setComPort(&comPort);
-  SysCall_setComPort(&comPort); // set this only after syscall isr is installed.
-	Screen s;
-	s.cls();
-	Idt idt;
-	Timer8253 timer8253;
-	::init_ISR(idt);
-	::init_IntCtrlr();
-	//printf("Booting hOS...\n");
-	//TRACE_HEX(pm_c_entry);
-	//asm("int $0x20\n\t");
-  //
-  // Before enabling interrupts. TSS has to be setup. 201111271528
-	Gdt gdt; // Copy original GDT and give more segment descriptor, TSS, user mode segs, etc.
-	gdt.initFromCpu();
+    ComPort comPort;
+    SysCall_setComPort(&comPort); // set this only after syscall isr is installed.
 
-  //CHECK_POINT;
-	Tss tss; // The single TSS that gives SS:SP for every privellege.
-  // We need not to init this TSS cause the task need no initialization.
-  // (It is running here)
-  /*
-#error init tss
-	tss.esp0 = ...;
-	tss.ss0 = 0x18;
-	tss.revAndTrapFlag = 0;
-	// other fields are irrelevant in my purpose.
-#error	here.
-  */
-	unsigned short tssSelector;
-	{ // This block to make some variable local in local.
-		SegDesc tssDesc; // This variable will be released from the stack after leaving this block.
-    //TRACE_HEX(sizeof(tss));
-    //TRACE_HEX(sizeof(Tss));
-		tssDesc.setTssSeg(&tss, sizeof(tss)-1, 0);
-		tssSelector = gdt.push(tssDesc);
-	}
+    Screen s;
+    s.cls();
 
-  unsigned short userCodeSelector;
-  {
-    SegDesc desc; // The code segment for user mode.
-    desc.setCodeSeg(SEGS_BASE, 0x20, 0x03);
-    userCodeSelector = gdt.push(desc) | 0x03;
-  }
+    Idt idt;
+    Timer8253 timer8253;
+    ::init_ISR(idt);
+    ::init_IntCtrlr();
 
-  unsigned short userStackSeletor;
-  {
-    SegDesc desc; // The code segment for user mode.
-    desc.setStackSeg(SEGS_BASE, 0x21, 0x03);
-    userStackSeletor = gdt.push(desc) | 0x03;
-  }
+    // Before enabling interrupts. TSS has to be setup. 201111271528
+    Gdt gdt; // Copy original GDT and give more segment descriptor, TSS, user mode segs, etc.
+    gdt.initFromCpu();
 
-  unsigned short sysCallGateSelector;
-  {
-    CallGateDesc desc; // the call gate descriptor for sys call.
-    desc.setCallGate(8 /* FIXME: evil magic number */, SysCall_syscall, 0x03, 1); 
-    sysCallGateSelector = gdt.push(*((SegDesc*)&desc)) | 0x03;
-    SysCall_u_setCallGateSelector(sysCallGateSelector);
-  }
+    Tss tss; // The single TSS that gives SS:SP for every privellege.
+    // We need not to init this TSS cause the task need no initialization.
+    // (It is running here)
+
+    unsigned short tssSelector;
+    { // This block to make some variable local in local.
+        SegDesc tssDesc; // This variable will be released from the stack after leaving this block.
+        //TRACE_HEX(sizeof(tss));
+        //TRACE_HEX(sizeof(Tss));
+        tssDesc.setTssSeg(&tss, sizeof(tss)-1, 0);
+        tssSelector = gdt.push(tssDesc);
+    }
+
+    unsigned short userCodeSelector;
+    {
+        SegDesc desc; // The code segment for user mode.
+        desc.setCodeSeg(SEGS_BASE, 0x20, 0x03);
+        userCodeSelector = gdt.push(desc) | 0x03;
+    }
+
+    unsigned short userStackSeletor;
+    {
+        SegDesc desc; // The code segment for user mode.
+        desc.setStackSeg(SEGS_BASE, 0x21, 0x03);
+        userStackSeletor = gdt.push(desc) | 0x03;
+    }
+
+    unsigned short sysCallGateSelector;
+    {
+        CallGateDesc desc; // the call gate descriptor for sys call.
+        desc.setCallGate(8 /* FIXME: evil magic number */, SysCall_syscall, 0x03, 1); 
+        sysCallGateSelector = gdt.push(*((SegDesc*)&desc)) | 0x03;
+        SysCall_u_setCallGateSelector(sysCallGateSelector);
+    }
 
 
-  gdt.setToCpu();
-  //TRACE_HEX(tssSelector);
-  //TRACE_HEX(&tss);
-	asm (
-      "movw %0, %%bx\n\t"
-			"ltrw %%bx\n\t"
-			::"g"(tssSelector):"%bx"
-	    );
+    gdt.setToCpu();
 
-	//::enable_Int();
+    asm (
+            "movw %0, %%bx\n\t"
+            "ltrw %%bx\n\t"
+            ::"g"(tssSelector):"%bx"
+        );
 
-  //CHECK_POINT;
-  //asm( "int $0x20\n\t" );
-  //idle((void*)0x333333);
-  int *idle_sp = (int *)(idle_stack + sizeof(idle_stack));
-  *(--idle_sp) = 0x333333; // the parameter to be passed into.
-  *(--idle_sp) = 0x000000; // The 'return address
-  //TRACE_HEX(userStackSeletor);
-  //TRACE_HEX(userCodeSelector);
+    Scheduler scheduler;
+    Scheduler::setScheduler(&scheduler);
+    Thread::setScheduler(&scheduler);
+    SysCall_u_setScheduler(&scheduler);
 
-  Scheduler scheduler;
-  Scheduler::setScheduler(&scheduler);
-  Thread::setScheduler(&scheduler);
-  SysCall_u_setScheduler(&scheduler);
-  IdleThread idleThread;
-  MyThread thread1(1); // create two threads
-  MyThread thread2(2);
+    IdleThread idleThread;
+    MyThread thread1(1); // create two threads
+    MyThread thread2(2);
 
-  TRACE_HEX(&thread1);
-  TRACE_HEX(&thread2);
+    TRACE_HEX(&thread1);
+    TRACE_HEX(&thread2);
 
-  ContextEsp ctxEsp;
-  ContextEbp *ctxEbp; // a dummy one for scheduler to fill in context.
+    ContextEsp ctxEsp;
+    ContextEbp *ctxEbp; // a dummy one for scheduler to fill in context.
 
 
 #if 0
-  int i = 0;
-  while(1)
-  {
-    TRACE_HEX(i++);
-  }
+    int i = 0;
+    while(1)
+    {
+        TRACE_HEX(i++);
+    }
 #endif
 
-  asm(
-      "mov %%ds, %%ax\n\t"
-      "xor $0x03, %%ax\n\t" // make the selector used in usermode has correct RPL.
-      "mov %%ax, %%ds\n\t"
-      "mov %%ax, %%es\n\t"
-      "mov %%ax, %%fs\n\t"
-      "mov %%ax, %%gs\n\t"
-      "pushl %1\n\t"  // push SS
-      "pushl $0\n\t"  // push ESP, going to be set by Scheduler::switchTo().
-      "pushl $0x02\n\t" // push EFLAG
-      "pushl %2\n\t" // push CS
-      "pushl $0\n\t" // push EIP, going to be set by Scheduler::switchTo().
-      "pushl $0\n\t" // This position is for ebp right after a function call.
-      "mov %%esp, %0\n\t" // get the pointer to ebp context. (In general function, mov %esp, %ebp right on entry)
-      :"=g"(ctxEbp)
-      :"g"((unsigned int)userStackSeletor),
-      "g"((unsigned int)userCodeSelector)
-      );
-  asm( // prepare stack space for function call of 'switchTo'.
-      "pushl $0\n\t"
-      "pushl $0\n\t"
-     );
-  Context ctx = { ctxEbp, &ctxEsp };
-  scheduler.start(&ctx); // !! Must iret right away to keep stack coherent.
-  asm( // clear stack space for function call of 'switchTo'.
-      "popl %eax\n\t"
-      "popl %eax\n\t"
-     );
-  asm( // store ss0 and esp0 to TSS.
-      "mov %%ss, %0\n\t"
-      "mov %%esp, %1\n\t"
-      :"=g"(tss.ss0), "=g"(tss.esp0)
-     );
-  asm(
-      "pop %ebp\n\t"
-      "iret\n\t" // 'return' to the idle function.
-     );
-  CHECK_POINT;
-	while(1)
-	{
-		//printf("-> %d \t", i++);
-	}
+    asm(
+            "mov %%ds, %%ax\n\t"
+            "xor $0x03, %%ax\n\t" // make the selector used in usermode has correct RPL.
+            "mov %%ax, %%ds\n\t"
+            "mov %%ax, %%es\n\t"
+            "mov %%ax, %%fs\n\t"
+            "mov %%ax, %%gs\n\t"
+            "pushl %1\n\t"  // push SS
+            "pushl $0\n\t"  // push ESP, going to be set by Scheduler::switchTo().
+            "pushl $0x02\n\t" // push EFLAG
+            "pushl %2\n\t" // push CS
+            "pushl $0\n\t" // push EIP, going to be set by Scheduler::switchTo().
+            "pushl $0\n\t" // This position is for ebp right after a function call.
+            "mov %%esp, %0\n\t" // get the pointer to ebp context. (In general function, mov %esp, %ebp right on entry)
+            :"=g"(ctxEbp)
+            :"g"((unsigned int)userStackSeletor),
+            "g"((unsigned int)userCodeSelector)
+       );
+    asm( // prepare stack space for function call of 'switchTo'.
+            "pushl $0\n\t"
+            "pushl $0\n\t"
+       );
+    Context ctx = { ctxEbp, &ctxEsp };
+    scheduler.start(&ctx); // !! Must iret right away to keep stack coherent.
+    asm( // clear stack space for function call of 'switchTo'.
+            "popl %eax\n\t"
+            "popl %eax\n\t"
+       );
+    asm( // store ss0 and esp0 to TSS.
+            "mov %%ss, %0\n\t"
+            "mov %%esp, %1\n\t"
+            :"=g"(tss.ss0), "=g"(tss.esp0)
+       );
+    asm(
+            "pop %ebp\n\t"
+            "iret\n\t" // 'return' to the idle function.
+       );
+    CHECK_POINT;
+    while(1)
+    {
+        //printf("-> %d \t", i++);
+    }
 }
